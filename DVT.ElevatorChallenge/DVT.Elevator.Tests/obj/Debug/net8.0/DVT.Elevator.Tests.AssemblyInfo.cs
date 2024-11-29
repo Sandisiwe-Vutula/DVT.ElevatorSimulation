d0f86c6fa96f1c96f442945cc0a48278b5a87f7f
Sandisiwe Vutula <sandisiwevutula28@gmail.com>
2024-11-29 20:13:52 +0200

    Set up GitHub Actions for automated build and tests

--- DVT.ElevatorChallenge/DVT.Elevator.Tests/obj/Debug/net8.0/DVT.Elevator.Tests.AssemblyInfo.cs
+++ DVT.ElevatorChallenge/DVT.Elevator.Tests/obj/Debug/net8.0/DVT.Elevator.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DVT.Elevator.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aba927b6bf3868e27ab939af68ff92aef6e4280f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad4e274b0af0e197207ac96dc5a69e1a3f8037a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("DVT.Elevator.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DVT.Elevator.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
